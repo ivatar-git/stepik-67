7932ff5d8bd34b2c64c216054ae7eeb254e9d647
ivatar-git <ivan.tarulin@gmail.com>
2021-07-19 13:53:57 +0300

    first try

--- lesson2-6(task#1).py
+++ lesson2-6(task#1).py
@@ -1,12 +1,12 @@
-sum = 0
-pow = 0
-while True:
+sum = 0 #переменная для суммы input
+pow = 0 #переменная суммы квадратов input
+while True: #бесконечный цикл
     a = int(input())
-    sum += a
+    sum += a #суммируем input
 
-    if sum == 0:
+    if sum == 0: #печатаем накопленный pow и разрываем цикл
         print(pow + a ** 2)
         break
 
-    else:
+    else: #суммируем pow
         pow += a ** 2
